501d8ed136935a2552861c0ed6852c85642925a6
Marciele <marcielerommel@gmail.com>
2023-08-29 17:19:28 -0300

    frist commit

---
+++ src/app/api/user.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  
+  private url: string = "https://api.adviceslip.com"
+  private options: any = { headers: new HttpHeaders({ 'Content-type': 'application/json; charset=UTF-8' }) };
+
+  // monta o link e pega os dados
+  constructor(private HTTP: HttpClient) { }
+  //metodo para pegar os dados GET
+  // colocar o path aqui
+  getData() {
+    return this.HTTP.get(`${this.url}todos`)
+  }
+
+}
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef } from '@angular/core';
+import {UserService} from '../../app/api/user.service';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
+  providers:[{provide: UserService}]
 })
-export class Tab1Page {
 
-  constructor() {}
+export class Tab1Page {
+  public listaConselhos = new Array<any>();
+
+  constructor(private UserService: UserService) {
+    this.getData();
+  }
+
+  getData() {
+    this.UserService.getData().subscribe(data => {
+      console.log(data);
+    });
+
+  }
 
 }
+
+
+
+
+
+ /* // aqui ele abre aquele pop-over alert
+  constructor() {
+    private UserProvider:UserProvider
+    //this.somaDoisnumeros(10,99);
+
+    }
+
+    ionViewDidLoad(){
+      this.UserProvider.getLatestApi()
+    }
+   /* public somaDoisnumeros(num1:number, num2:number): void {
+
+      // tem o alert para trazer o pop-over em tela
+      alert (num1+num2);*/
